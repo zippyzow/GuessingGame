b0d5506fe59598efb4383f94162cde66ac334b3e
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2015-12-01 15:41:29 -0500

    Added javascript functions and jQuery submit button

--- GuessingGame.js
+++ GuessingGame.js
@@ -1,3 +1,27 @@
 /**
  * Created by haileykeen on 11/22/15.
  */
+/* **** Guessing Game Functions **** */
+
+// Generate the Winning Number
+
+function generateWinningNumber() {
+  return Math.floor((Math.random() * 100) + 1);
+}
+
+// Fetch the Players Guess
+
+function playersGuessSubmission() {
+
+}
+
+
+
+/* **** Event Listeners/Handlers ****  */
+
+// Clicking Submit button
+$(".submit-button").click(function() {
+  console.log("Clicked");
+});
+
+
