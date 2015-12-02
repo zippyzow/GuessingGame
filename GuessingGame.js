4ce46ed862d56c52242a3f1375fbfef9665b94a9
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2015-12-01 19:15:21 -0500

    Hooked up submit button, start over button

--- GuessingGame.js
+++ GuessingGame.js
@@ -1,37 +1,83 @@
 /**
  * Created by haileykeen on 11/22/15.
  */
-/* **** Guessing Game Functions **** */
 
-// Generate the Winning Number
+/** Global Variables */
 
+var winningNumber = generateWinningNumber();;
+
+
+/** Guessing Game Functions */
+
+/**
+ * Returns randomly generated number.
+ * @returns {Number} what player has to guess to win game.
+ */
 function generateWinningNumber() {
   return Math.floor((Math.random() * 100) + 1);
 }
 
-// Fetch the Players Guess
-
+/**
+ * Returns the guess that the player has entered in the input.
+ * @returns {Number} the player's guess.
+ */
 function getPlayerGuess() {
-  //player enters guess in input field
+  return $(".guess-input").val();
+}
+
+/**
+ * Returns a number that signfies how far away the player's guess is from the winning number.
+ * If the number is positive the guess is too high.
+ * If the number is negative the guess is too low.
+ * If the number is zero, the guess is correct.
+ * @returns {Number} the difference between the player's guess and the winning number.
+ */
+function checkGuess() {
+  return getPlayerGuess() - winningNumber;
+}
+
+/**
+ * Sets the text of the tint tooltip.
+ */
+function setHintText() {
+  $(".hint-text").text("The answer is: " + winningNumber);
+}
+
+/** Event Handlers */
+
+/**
+ * Alerts player after submission whether their guess is too low, too high, or correct.
+ */
+function handleSubmitButtonClick() {
+  var difference = checkGuess();
+  if (difference < 0) {
+    alert("Your guess is too low");
+  } else if (difference > 0) {
+    alert("Your number is too high");
+  } else {
+    alert("You won!");
+  }
+}
+
+/**
+ * Refreshes the page.
+ */
+function handleStartOverButtonClicked() {
+  // Refreshes page.
+  window.location.reload();
 }
 
 
-
-
-/* **** Event Listeners/Handlers ****  */
+/** DOM Ready Code */
 
 $(function() {
+  setHintText();
 
-  // Clicking Submit button
-  $(".submit-button").click(function () {
-    console.log("Clicked");
+  $(".submit-button").click(function() {
+    handleSubmitButtonClick();
   });
 
-  // Entering in guess into input field 
-  $(".guess-input").keyup(function () {
-    console.log("Typed");
-  })
-
+  $(".start-over-button").click(function() {
+    handleStartOverButtonClicked();
+  });
 });
-
-
