dfec4bf82a6306900beb86410ed6ece8bc6276e4
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2015-12-04 23:58:14 -0500

    fixed bugs

--- GuessingGame.js
+++ GuessingGame.js
@@ -5,13 +5,14 @@
 /** Global Variables */
 
 var winningNumber = generateWinningNumber();;
+var turnCounter = 0;
 
 
 /** Guessing Game Functions */
 
 /**
  * Returns randomly generated number.
- * @returns {Number} what player has to guess to win game.
+ * @returns {number} what player has to guess to win game.
  */
 function generateWinningNumber() {
   return Math.floor((Math.random() * 100) + 1);
@@ -19,10 +20,10 @@ function generateWinningNumber() {
 
 /**
  * Returns the guess that the player has entered in the input.
- * @returns {Number} the player's guess.
+ * @returns {number} the player's guess.
  */
 function getPlayerGuess() {
-  return $(".guess-input").val();
+  return $('.guess-input').val();
 }
 
 /**
@@ -30,7 +31,7 @@ function getPlayerGuess() {
  * If the number is positive the guess is too high.
  * If the number is negative the guess is too low.
  * If the number is zero, the guess is correct.
- * @returns {Number} the difference between the player's guess and the winning number.
+ * @returns {number} the difference between the player's guess and the winning number.
  */
 function checkGuess() {
   return getPlayerGuess() - winningNumber;
@@ -40,25 +41,64 @@ function checkGuess() {
  * Sets the text of the tint tooltip.
  */
 function setHintText() {
-  $(".hint-text").text("The answer is: " + winningNumber);
+  $('.hint-text').text('The answer is: ' + winningNumber);
 }
 
 /** Event Handlers */
 
+function validateSubmission() {
+  var val = parseInt($('.guess-input').val());
+  return typeof val === 'number' && 1 <= val && val <= 100;
+}
+
 /**
  * Alerts player after submission whether their guess is too low, too high, or correct.
  */
-function handleSubmitButtonClick() {
+function handlePlayerInput() {
+  useTurn();
+  
   var difference = checkGuess();
+  var message = "";
   if (difference < 0) {
-    alert("Your guess is too low");
+    message += 'Your guess is too low.';
   } else if (difference > 0) {
-    alert("Your number is too high");
+    message += 'Your number is too high.';
   } else {
-    alert("You won!");
+    message += 'You won!';
   }
+
+  var absDifference = Math.abs(difference);
+  if (absDifference > 0) {
+    if (absDifference <= 5) {
+      message += " You are hot!";
+    } else if (absDifference <= 15) {
+      message += " You are warm.";
+    } else if (absDifference <= 30) {
+      message += " You are cold.";
+    } else {
+      message += " You are freezing!";
+    }
+  }
+
+  showMessage(message);
 }
 
+/** 
+ * Updates the turn-indicator view and increments turnCounter after each turn
+*/
+function useTurn() {
+  $('.circle-container').children().eq(turnCounter).addClass('empty-circle');
+  turnCounter++;
+}
+
+/**
+ * Displays a temperature message to the user.  
+ * @param {string} message The message to display.
+ */
+function showMessage(message) {
+  $('.temperature').text(message).slideDown(175);
+
+}
 /**
  * Refreshes the page.
  */
@@ -73,11 +113,21 @@ function handleStartOverButtonClicked() {
 $(function() {
   setHintText();
 
-  $(".submit-button").click(function() {
-    handleSubmitButtonClick();
+  $('.submit-button').click(function() {
+    handlePlayerInput();
   });
 
-  $(".start-over-button").click(function() {
+  $('.start-over-button').click(function() {
     handleStartOverButtonClicked();
+  });
+
+  $('.guess-input').keyup(function() {
+    $('.submit-button').prop("disabled", !validateSubmission());
+  });
+
+  $('.guess-input').keypress(function(e) {
+    if (e.which == 13) {
+      handlePlayerInput();
+    }
   });
 });
